3be5fca2daf7e22604fef294aaf9445fd3d4bc9d
johnwgomez <johnwgomez10@gmail.com>
2025-03-20 19:19:42 -0700

    made motorbike extend to vehicle

--- dist/classes/Motorbike.js
+++ dist/classes/Motorbike.js
@@ -1,5 +1,11 @@
+import Vehicle from './Vehicle.js'; // Import Vehicle class
+
 // TODO: The Motorbike class should extend the Vehicle class
-class Motorbike {
+class Motorbike extends Vehicle { // Extend Motorbike from Vehicle
+    constructor(make, model, year) {
+        super(make, model, year); // Call parent constructor
+    }
 }
+
 // Export the Motorbike class as the default export
 export default Motorbike;
